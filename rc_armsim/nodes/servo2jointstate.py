434bb4a1b24ce697de65e8237f136dc7c96ec38a
Jesús Arturo Escobedo Cabello <arturoescobedo.iq@gmail.com>
2017-09-26 18:52:59 -0500

    servo2jointstate.py adding remaping function to adapt servo angle values to robot configuration

--- rc_armsim/nodes/servo2jointstate.py
+++ rc_armsim/nodes/servo2jointstate.py
@@ -13,7 +13,9 @@ class Servo2JointState():
         """ Servo2JointState
             
             This node reads the angles in DEGREES from 3 topics called servo0, servo1, servo2 
-            and publishes those values to the /joint_input topic used by ros simulator
+            and publishes those values to the /joint_input topic used by ros simulator.
+            The values of the angles will be constrained to the dimensions of the palletizing robot 
+            used in class.
         """
         """ Data
         """
@@ -43,15 +45,32 @@ class Servo2JointState():
     def servo0Cb(self, data):
         """ This callback is executed when we receive a message in servo0 topic
         """
-        self.servo0_angle = data.data
+        lower_limit=0
+        higher_limit=180
+        sim_lower_limit=-90
+        sim_higher_limit=90
+        self.servo1_angle=np.interp(data.data,[lower_limit,higher_limit],[sim_lower_limit,sim_higher_limit])
+
+            
     def servo1Cb(self, data):
         """ This callback is executed when we receive a message in servo1 topic
         """
-        self.servo1_angle = data.data
+        lower_limit=0
+        higher_limit=180
+        sim_lower_limit=-90
+        sim_higher_limit=90
+        self.servo1_angle=np.interp(data.data,[lower_limit,higher_limit],[sim_lower_limit,sim_higher_limit])
+
+    
     def servo2Cb(self, data):
         """ This callback is executed when we receive a message in servo2 topic
         """
-        self.servo2_angle = data.data
+        lower_limit=0
+        higher_limit=180
+        sim_lower_limit=-90
+        sim_higher_limit=90
+        self.servo2_angle=np.interp(data.data,[lower_limit,higher_limit],[sim_lower_limit,sim_higher_limit])
+
         
 if __name__ == "__main__":
     rospy.init_node('servo2jointstate', anonymous=True)
