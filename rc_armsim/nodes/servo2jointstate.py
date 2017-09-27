118a607a3423ccf98113e410aea18cad2ac653c9
Jesús Arturo Escobedo Cabello <arturoescobedo.iq@gmail.com>
2017-09-27 16:00:00 -0500

    modifying arm.xacro from 4DOF to  3DOF arm, modifying arm_rviz.launch to launch servo2jointstate.py, modifying servo2jointstate.py to be used with the 3DOF arm

--- rc_armsim/nodes/servo2jointstate.py
+++ rc_armsim/nodes/servo2jointstate.py
@@ -5,8 +5,7 @@ import numpy as np
 from std_msgs.msg import UInt16
 from sensor_msgs.msg import JointState
 
-def servo0Cb(data):
-    var=UInt16()
+
 class Servo2JointState():
     
     def __init__(self):
@@ -14,7 +13,7 @@ class Servo2JointState():
             
             This node reads the angles in DEGREES from 3 topics called servo0, servo1, servo2 
             and publishes those values to the /joint_input topic used by ros simulator.
-            The values of the angles will be constrained to the dimensions of the palletizing robot 
+            The values of the angles will be constrained to the dimensions of simulated robot 
             used in class.
         """
         """ Data
@@ -22,8 +21,7 @@ class Servo2JointState():
         self.servo0_angle = 0 #angle must be in degrees
         self.servo1_angle = 0 #angle must be in degrees
         self.servo2_angle = 0 #angle must be in degrees
-        
-        
+        self.gripper_angle =0 #angle must be in degrees
         """ Publishers
         """
         self.joint_input_pub = rospy.Publisher('joint_input', JointState, queue_size=10)
@@ -32,13 +30,14 @@ class Servo2JointState():
         rospy.Subscriber('servo0', UInt16, self.servo0Cb)
         rospy.Subscriber('servo1', UInt16, self.servo1Cb)
         rospy.Subscriber('servo2', UInt16, self.servo2Cb)
+        rospy.Subscriber('servogripper', UInt16, self.gripperCb)
     
         rate = rospy.Rate(10) # 10hz
         state = JointState()
         count=-1.5
         while not rospy.is_shutdown():
-            state.name=["base_to_link1", "link1_to_link2", "link2_to_link3"]
-            state.position=[np.deg2rad(self.servo0_angle), np.deg2rad(self.servo1_angle), np.deg2rad(self.servo2_angle)]
+            state.name=["base", "arti1", "arti2", "gripper"]
+            state.position=[np.deg2rad(self.servo0_angle), np.deg2rad(self.servo1_angle), np.deg2rad(self.servo2_angle),  np.deg2rad(self.gripper_angle)]
             self.joint_input_pub.publish(state)
             rate.sleep()
         
@@ -49,7 +48,7 @@ class Servo2JointState():
         higher_limit=180
         sim_lower_limit=-90
         sim_higher_limit=90
-        self.servo1_angle=np.interp(data.data,[lower_limit,higher_limit],[sim_lower_limit,sim_higher_limit])
+        self.servo0_angle=np.interp(data.data,[lower_limit,higher_limit],[sim_lower_limit,sim_higher_limit])
 
             
     def servo1Cb(self, data):
@@ -71,7 +70,15 @@ class Servo2JointState():
         sim_higher_limit=90
         self.servo2_angle=np.interp(data.data,[lower_limit,higher_limit],[sim_lower_limit,sim_higher_limit])
 
-        
+    def gripperCb(self, data):
+        """ This callback is executed when we receive a message in servo2 topic
+        """
+        lower_limit=0
+        higher_limit=180
+        sim_lower_limit=-90
+        sim_higher_limit=90
+        self.gripper_angle=np.interp(data.data,[lower_limit,higher_limit],[sim_lower_limit,sim_higher_limit]) 
+
 if __name__ == "__main__":
     rospy.init_node('servo2jointstate', anonymous=True)
     try:
