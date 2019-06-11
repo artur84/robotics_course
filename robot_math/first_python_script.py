1c6bf5ea4deda4aefc8b44cf2e91c69d63b0d34a
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2019-06-11 10:57:03 -0500

    using numpy

--- robot_math/first_python_script.py
+++ robot_math/first_python_script.py
@@ -1,1 +1,22 @@
-print("hola")
+import numpy as np
+import pandas as pd
+np.zeros((3,4))
+A=np.ones((3,3))
+I=np.eye(3)
+np.empty((3,3))
+X=np.array([[1,2,3],[2,2,2],[3,3,3]])
+Y=np.array([[1,5,2],[0,0,0],[0,0,0]])
+a-b
+v=np.array([[1],[3],[0]])
+X@v
+XI=np.linalg.pinv(X)
+XI@I
+
+def rotx(angle,type="rad"):
+    if type=="deg":
+        R=np.array([[np.cos(np.deg2rad(angle)),-np.sin(np.deg2rad(angle)),0],[np.sin(np.deg2rad(angle)),np.cos(np.deg2rad(angle)),0],[0,0,1]])
+    else:
+        R=np.array([[np.cos(angle),-np.sin(angle),0],[np.sin(angle),np.cos(angle),0],[0,0,1]])
+    return R
+
+R=rotx(30,"deg")
