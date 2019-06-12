55ad4ef42209b462e84d62b9f79a9f26468fb4ab
Jesus-Arturo Escobedo-Cabello <arturoescobedo.iq@gmail.com>
2019-06-12 12:13:07 -0500

    Applying first_python_script.py

--- robot_math/scripts/first_python_script.py
+++ robot_math/scripts/first_python_script.py
@@ -2,22 +2,30 @@ import numpy as np
 
 np.zeros((3, 4))
 A = np.ones((3, 3))
-I = np.eye(3)
+I3 = np.eye(3)
 np.empty((3, 3))
-X = np.array([[1,2,3],[2,2,2],[3,3,3]])
-Y=np.array([[1,5,2],[0,0,0],[0,0,0]])
+X = np.array([[1, 2, 3], [2, 2, 2], [3, 3, 3]])
+Y = np.array([[1, 5, 2], [0, 0, 0], [0, 0, 0]])
 
-v=np.array([[1],[3],[0]])
+v = np.array([[1], [3], [0]])
 X@v
-XI=np.linalg.pinv(X)
-XI@I
+XI = np.linalg.pinv(X)
+XI@I3
 
-def rotx(angle,type="rad"):
-    if type=="deg":
-        R=np.array([[np.cos(np.deg2rad(angle)),-np.sin(np.deg2rad(angle)),0],[np.sin(np.deg2rad(angle)),np.cos(np.deg2rad(angle)),0],[0,0,1]])
+
+def rotx(angle, type="rad"):
+    if type == "deg":
+        R = np.array([[np.cos(np.deg2rad(angle)), -np.sin(np.deg2rad(angle)), 0],
+                      [np.sin(np.deg2rad(angle)), np.cos(np.deg2rad(angle)), 0],
+                      [0, 0, 1]])
     else:
-        R=np.array([[np.cos(angle),-np.sin(angle),0],[np.sin(angle),np.cos(angle),0],[0,0,1]])
+        R = np.array([[np.cos(angle), -np.sin(angle), 0],
+                      [np.sin(angle), np.cos(angle), 0], [0, 0, 1]])
     return R
 
-R=rotx(30,"deg")
-print(R)
+
+""" Compute Rotation Matrices
+"""
+Rx = rotx(30, "deg")  # R
+
+print(Rx)
