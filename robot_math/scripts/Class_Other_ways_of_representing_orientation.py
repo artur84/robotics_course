242d179b2a466dcb5c54ebb5efa31d19c2ec909a
Jesus Arturo Escobedo Cabello <arturoescobedo.iq@gmail.com>
2023-02-28 15:33:21 -0600

    Class other ways of representing orientation

--- robot_math/scripts/Class_Other_ways_of_representing_orientation.py
+++ robot_math/scripts/Class_Other_ways_of_representing_orientation.py
@@ -8,57 +8,61 @@ from numpy.linalg import inv
 
 phi, Q, psi = sym.symbols('phi Q psi') #Create some symbolic variables
 
-#Create a rotation matrix about x
-rotx=SO3.Rx(Q) #High level structure
-#rotx=tr.rotx(Q) #Low level structure (numpy array)
-print("Rx")
-print(rotx)
-#print(rotx)
-
-#Create a rotation matrix about y
-Rotx=SO3.Rx(Q) #High level structure
-#rotx=tr.rotx(Q) #Low level structure (numpy array)
-print("Rx")
-print(Rotx)
-#print(rotx)
-
-#Create a rotation matrix argument in radians
-R_pi = SO3.Rx(np.pi)
-R_pi.print()
-
-#Create a rotation matrix in degrees
-R_30 = SO3.Rx(30,'deg')
-R_30.print()
-
-R_minus30 = SO3.Rx(-30, 'deg')
-R_minus30.print()
-
-# Inverse of a rotation matrix 
-R30_inv=R_30.inv()
-print("inverse of Rx")
-
-print(R30_inv)
-
-# Determinant of a rotation matrix (using numpy.linalg.det function)
-det(R_30)
-
-#Rotation about y-axis
-Ry=SO3.Ry(B)
-print("Ry")
-print(Ry)
+#Create a rotation matrix for Euler angles ZXZ
+R=SO3.Rz(phi)*SO3.Rx(Q)*SO3.Rz(psi)
+R.print()
 
 
-# Rotation about z-axis
-#Rz=tr.rotz(a)
-Rz=SO3.Rz(a)
-print("Rz")
-print(Rz)
+#Create a Rotation matrix Rz(30)Rx(60)Rz(45)
+R1=SO3.Rz(30,'deg')*SO3.Rx(60,'deg')*SO3.Rz(45,'deg')
+R1.print()
 
-detx = Rotx_inv.norm()
-print("R")
+
+#Create a Rotation matrix Rz(30)Rx(60)Rz(45)
+R2=SO3.Rz(45,'deg')*SO3.Rx(60,'deg')*SO3.Rz(30,'deg')
+R2.print()
+
+#From R to Euler ZXZ
+phi, Q, psi = sym.symbols('phi Q psi') #Create some symbolic variables
+sx, sy, sz = sym.symbols('sx sy sz') #Create some symbolic variables
+ax, ay, az = sym.symbols('ax ay az') #Create some symbolic variables
+nx, ny, nz = sym.symbols('nx ny nz') #Create some symbolic variables
+
+R=np.array([[sx, nx, ax],[sy, ny, ay],[sz, nz, az]])
 print(R)
 
-#print("simplify R")
-#print(sym.simplify(R))
+print(SO3.Rz(-phi)*R)
+print(SO3.Rx(Q)*SO3.Rz(psi))
 
+#Example ZXZ2 Compute ZXZ Euler Angles from Rotation Matrix
 
+R1=np.array(SO3.Rz(30,'deg')*SO3.Rx(60,'deg')*SO3.Rz(45,'deg'))
+sx=R1[0][0]
+sy=R1[1][0]
+sz=R1[2][0]
+
+nx=R1[0][1]
+ny=R1[1][1]
+nz=R1[2][1]
+
+ax=R1[0][2]
+ay=R1[1][2]
+az=R1[2][2]
+
+phi1=np.arctan2(ax,-ay)
+print(np.rad2deg(phi1))
+
+phi2=np.arctan2(-ax,ay)
+print(np.rad2deg(phi2))
+
+theta1=np.arctan2(-ay*np.cos(phi1)+ax*np.sin(phi1),az)
+print(np.rad2deg(theta1))
+
+theta2=np.arctan2(-ay*np.cos(phi2)+ax*np.sin(phi2),az)
+print(np.rad2deg(theta2))
+
+psi1=np.arctan2(-nx*np.cos(phi1)-ny*np.sin(phi1),sx*np.cos(phi1)+sy*np.sin(phi1))
+print(np.rad2deg(psi1))
+
+psi2=np.arctan2(-nx*np.cos(phi2)-ny*np.sin(phi2),sx*np.cos(phi2)+sy*np.sin(phi2))
+print(np.rad2deg(psi2))
